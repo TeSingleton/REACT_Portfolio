d3e12d968197c12034ff06b7c4115e5e24c2f95a
Te <worksingletonmcdaniel@gmail.com>
2022-10-20 12:07:33 -0700

    adding changes to app header and footer js

---
+++ react_portfolio/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React, { Component }  from 'react';
+
+export const Footer =()=>{
+    return(
+        <footer className="Footer"> Created With ❤️ by Dop.r Web Products</footer>
+    )
+}
---
+++ react_portfolio/src/components/Header.js
@@ -0,0 +1,7 @@
+import React, { Component }  from 'react';
+
+export const Header =()=>{
+    return(
+        <h1 className="Header">This is a Header</h1>
+    )
+}
