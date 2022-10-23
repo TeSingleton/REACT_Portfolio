0fab06bc592577b7cdedadbc7a9f483d3bffe517
Te <worksingletonmcdaniel@gmail.com>
2022-10-22 21:39:25 -0700

    fixing styling

--- react_portfolio/src/components/Footer.js
+++ react_portfolio/src/components/Footer.js
@@ -4,13 +4,13 @@ import TwitterIcon from "../images/iconmonstr-twitter-2-24.png"
 import LinkedinIcon from "../images/iconmonstr-linkedin-2-24.png"
 export const Footer =()=>{
     return(
-        <footer className="Footer"> 
-        <ul>
-            <li className='github '><a href='https://github.com/TeSingleton'><img className='githubIcon' src={GithubIcon} />GitHub</a></li>
+        <footer className="Footer">  
+        <ul> 
+            <li className='github '><a href='https://github.com/TeSingleton'><img className='githubIcon' src={GithubIcon} /></a></li>
             
-            <li className='linkedin'><a href='https://twitter.com/BigTe_'><img className='linkedIcon' src={LinkedinIcon}/>Linkedin</a></li>
+            <li className='linkedin'><a href='https://twitter.com/BigTe_'><img className='linkedIcon' src={LinkedinIcon}/></a></li>
 
-            <li className='twitter'><a href='https://www.linkedin.com/in/svanté-singleton-mcdaniel-90328260/'><img className='twitterIcon' src={TwitterIcon}/>Twitter</a></li>
-        </ul>Created With ❤️ by Dop.r Web Products</footer>
+            <li className='twitter'><a href='https://www.linkedin.com/in/svanté-singleton-mcdaniel-90328260/'><img className='twitterIcon' src={TwitterIcon}/></a></li>
+        </ul>Reach Out And Connect </footer>
     )
 }
